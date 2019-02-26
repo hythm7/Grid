3dd651d7496d3163ef2a7a376d428d847cb41235
Haythem Elganiny <haythem.elganiny@moorecap.com>
2019-02-26 10:30:20 -0500

    rm Grid::Util

--- lib/Grid.pm
+++ lib/Grid.pm
@@ -1,5 +1,3 @@
-use Grid::Util;
-
 unit role Grid[:$columns];
   
 has Int $!columns;
@@ -376,6 +374,48 @@ submethod !subgrid( @indices, :$square = False ) {
     return Array;
 
   }
+
+}
+
+
+sub diagonal ( @perfect --> Array ) is export {
+  #TODO: check if not perfect square
+
+  my $root = @perfect.sqrt.Int;
+
+  sub diagonaled-index ( Int $index ) { 
+    return $index when $index == @perfect.end;
+    return $index * $root mod @perfect.end;
+  }
+
+  my @diagonaled = @perfect[ @perfect.keys.map: *.&diagonaled-index ];
+
+  @diagonaled;
+
+}
+
+
+
+sub antidiagonal ( @perfect --> Array) is export {
+  my $root = @perfect.sqrt.Int;
+
+  multi antidiagonal-index ( Int $index ) {
+    my $newindex = @perfect.end - $index * $root;
+
+    return $newindex unless $newindex < 0;
+    samewith $newindex;
+  }
+
+  multi antidiagonal-index (Int $index where * < 0) {
+    my $newindex = $index + @perfect.end;
+    return $newindex unless $newindex < 0;
+    samewith $newindex;
+  }
+
+
+  my @antidiagonaled = @perfect[ @perfect.keys.map: *.&antidiagonal-index ];
+  
+  @antidiagonaled;
 
 }
 
